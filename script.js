8dd278804bcd235564277d238279e963e6225140
luja66888 <lucasjaegerpc@outlook.dk>
2025-05-26 10:53:52 +0200

    Rettelser overalt

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* HAMBURGERMENU */
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle =
 document.querySelector('.menu__toggle');
@@ -8,3 +9,20 @@ document.querySelector('.nav__links');
         navLinks.classList.toggle('active');
     });
     });
+
+
+/* FOOTER KNAPPER */
+var coll = document.getElementsByClassName("button__collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
